4599b242a459ad2c6aadf12c2974036f4ffd0bfc
HanenFourati <fouratihanen@gmail.com>
2020-03-21 15:13:52 +0100

    Updates

---
+++ precache-manifest.948d8cc5b0b617bb42e798392c19e0c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6e4601d0fbd2690a81ad4c630e55d11",
+    "url": "/startingapp/index.html"
+  },
+  {
+    "revision": "b12df2aa6e228df7b830",
+    "url": "/startingapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "232873d4b5abed31dd36",
+    "url": "/startingapp/static/js/2.0b5e69ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/startingapp/static/js/2.0b5e69ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b12df2aa6e228df7b830",
+    "url": "/startingapp/static/js/main.712f2b23.chunk.js"
+  },
+  {
+    "revision": "d9668cca99ba03852e0e",
+    "url": "/startingapp/static/js/runtime-main.b270e3de.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/startingapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/githosting/precache-manifest.9e9921e0e3899de18ad39a02aedef5e9.js"
+  "/startingapp/precache-manifest.948d8cc5b0b617bb42e798392c19e0c2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/githosting/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/startingapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
